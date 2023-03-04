043e986e81cbbe39d802bd65fdad5ea4712aaa1d
Mimi <trish21215@yahoo.com>
2023-03-04 12:00:15 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log('GigaPet')
+
+// "." is for classes "#" is for id's 
+
+
+// Creating a class for my Gigapet
+const tamagotchi = document.createElement('div')
+document.body.appendChild('.giga')
