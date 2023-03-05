49c0a1b351b7f67fc005375bb2346abc3f5ed6ec
Mimi <trish21215@yahoo.com>
2023-03-04 21:44:27 -0500

    NewGigapet

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-
+# Tamagotchi-Project
+As a player, I want to easily interact with my pet.
+I'd like to feed and play with my pet.
+I'd like to turn off the lights to signify bedtime.
+I would also like some custimization like naming the pet and celebrating birthdays.
--- js/main.js
+++ js/main.js
@@ -5,4 +5,81 @@ console.log('GigaPet')
 
 // Creating a class for my Gigapet
 const tamagotchi = document.createElement('div')
-document.body.appendChild('.giga')
+tamagotchi.className = 'giga'
+document.body.appendChild(tamagotchi)
+
+
+
+// function render() {
+// 	renderHands();
+// 	renderControls();
+// 	if (winner) {
+// 		renderWinnerMessage();
+// 	} else {
+// 		renderTurnMessage();
+// 	}
+// }
+
+// class Person {
+//     //   // constructor function, 
+//     //   // reservered word
+//       constructor(name) {
+//     //     // this is where we define our keys for the objects
+//     //     // to be created from this class
+//         this.name = name;
+//         this.isHungry = true;
+//         this.pockets = ['wallet'];
+//       }
+    
+//       eat() {
+//         console.log(this, " <- this");
+//         this.isHungry = false
+//       }
+    
+
+//Avoid accessing the DOM from outside render-oriented functions. However, "eye candy" animations, a ticking time display, etc. are exceptions to this tip.
+// Data (state) is the single source of truth of the app - when implementing an app's logic, the DOM is secondary to data manipulation. Get used to thinking about how to your app's data changes vs. the display.
+// As the user interacts with the application (or other events such as timers trigger), code the app such that it:
+// Updates state, then...
+// Calls render()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*----- constants -----*/
+
+
+  /*----- state variables -----*/
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
